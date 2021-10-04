6a2e39a9dd0e977e8810a12c7814934c8cace728
Hiroshige Arishima <bambangmahardhika1403@gmail.com>
2021-10-04 19:05:27 +0700

    ping pong done

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ require('dotenv').config({})
 
 const Discord   = require('discord.js'),
       {Intents} = require('discord.js')
-const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] })
+const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 bot.on("ready",()=>{
   console.log("ON !");
 })
 
-bot.on("messageCreate",message=>{
-  console.log(message.content);
-  // if(message.content[0] == '!'){
-  //   console.log(message);
-  //   switch(message.content.slice(1,-1)){
-  //     case "ping" : 
-  //       message.reply("kaget")
-  //   }
-  // }
+bot.on("messageCreate",(message)=>{
+  // console.log(message.content);
+  if(message.content == '!ping'){
+    message.channel.send('Pong')
+    // console.log(message.channel);
+  }
 });
 
 bot.login(process.env.TOKEN)
