c1dc4ffede0f69751baa62c03c88e0af998682b5
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2021-10-09 11:54:09 +0700

    revert bentar lagi

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ require('dotenv').config({})
 
 const Discord   = require('discord.js'),
       {Intents} = require('discord.js')
-const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] })
 
 bot.on("ready",()=>{
   console.log("ON !");
 })
 
+<<<<<<< HEAD
 prefix = '!'
 
 bot.on("messageCreate",(message)=>{
@@ -18,5 +19,17 @@ bot.on("messageCreate",(message)=>{
       case "ping" : 
         message.reply("kaget")
     }}});
+=======
+bot.on("messageCreate",message=>{
+  console.log(message.content);
+  // if(message.content[0] == '!'){
+  //   console.log(message);
+  //   switch(message.content.slice(1,-1)){
+  //     case "ping" : 
+  //       message.reply("kaget")
+  //   }
+  // }
+});
+>>>>>>> parent of 6a2e39a (ping pong done)
 
 bot.login(process.env.TOKEN)
