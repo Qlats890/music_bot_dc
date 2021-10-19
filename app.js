83a6c28f3c9d21c721eb0618842045016695906e
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2021-10-19 14:21:36 +0700

    nyoba command handler

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
 require('dotenv').config({})
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const Discord   = require('discord.js'),
-      {Intents} = require('discord.js')
-const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-bot.on("ready",()=>{
-  console.log("ON !");
-})
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
 
-prefix = '!'
 
-bot.on("messageCreate",(message)=>{
-  console.log(message.content)
-  if(message.content[0] === prefix){
-    console.log(message.content)
-    switch(message.content.substring(1,message.content.length)){
-      case "ping" : 
-        message.reply("kaget")
-    }}});
 
-bot.login(process.env.TOKEN)
+
+
+
+
+
+
+// require('dotenv').config({})
+
+// const Discord   = require('discord.js'),
+//       {Intents} = require('discord.js')
+// const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+// bot.on("ready",()=>{
+//   console.log("ON !");
+// })
+
+// prefix = '!'
+
+// bot.on("messageCreate",(message)=>{
+//   console.log(message.content)
+//   if(message.content[0] === prefix){
+//     console.log(message.content)
+//     switch(message.content.substring(1,message.content.length)){
+//       case "ping" : 
+//         message.reply("kaget")
+//     }}});
+
+// bot.login(process.env.TOKEN)
